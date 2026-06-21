4ca2895aa9712d2feb99215bb88e2c0b2f4e6c34
yahyaelsf <yahyaelsaftawi98@gmail.com>
2026-06-21 09:52:52 +0300

    first lec

---
+++ lec1.py
@@ -0,0 +1,36 @@
+# name = "yahya"
+# name = input("Enter your name :")
+# print(name)
+# x = 5
+# y = 3.14
+# print(type(x))
+# print(type(y))
+# is_single = True
+# print(type(is_single))
+# x = 5 % 2 
+# x = 5 + 3j #imag 
+# print(x.imag)
+# print(max(1 , 2 , -5 , 10))
+# x = 2.1
+# import math
+# print(math.floor(x))
+# name = "**Yahya El Saftawi**"
+# print(name[5:-1])
+# print(name.strip("*"))
+# x = 5
+# y = str(x)
+# print(type(y))
+# first_name = "yahya"
+# last_name = "elsaftawi"
+# full_name = first_name + " "+ last_name
+# print(f"Welcomr {first_name} ssss {last_name}")
+# yahya@gmail.com
+# email = input("Enter your Email :")
+# ind_one = email.find("@")
+# ind_tow = email.find(".")
+# print(email[:ind_one])
+# print(email[ind_one + 1:ind_tow])
+# print(email[ind_tow + 1:])
+length = 170 
+wight = 60 
+print( not wight >= 60)
